b5cd6b7e25e70b6f7abce1253b18e63aa20b1876
Tahtawy <mght29@gmail.com>
2018-11-06 11:10:35 +0200

    main controller logic

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -2,13 +2,16 @@
     var app = angular.module("githubApp");
 
     var MainController = function ($scope, github) {
-        
+
         github.getUsers().then(function (data) {
             $scope.users = data;
         });
-
-        github.getUsers(true).then(function (data) {
-            $scope.first = data;
+        
+        github.getUsers(true).then(function (data) { // Set Current User
+            github.setCurrentUser(data);
+            github.getCurrentUser().then(function(data) {
+                $scope.currentUser = data;
+            });
         });
 
     };
--- js/services/GithubService.js
+++ js/services/GithubService.js
@@ -2,22 +2,36 @@
 
     var github = function ($http) {
         
+        var currentUser = {};
+
         var getUsers = function (firstUser) {
             return $http.get('https://api.github.com/users').then(function (response) {
-                if(firstUser) {
-                    return response.data[0].login;  
-                } 
-                return response.data;
+                if (firstUser) {
+                    return response.data[0].login;
+                }
+                return response.data; // All Users
             });
         };
 
+        var setCurrentUser = function (login) {
+            currentUser = $http.get('https://api.github.com/users/'+login).then(function (response) {
+                return response.data;
+            });
+        }
+
+        var getCurrentUser = function () {
+            return currentUser;
+        }
+
         return {
-            getUsers: getUsers
+            getUsers: getUsers,
+            setCurrentUser: setCurrentUser,
+            getCurrentUser: getCurrentUser
         };
 
     };
 
-    var app = angular.module('githubApp');
+    var app = angular.module('githubApp'); // Get Referance To Module
     app.factory('github', github); // Register the service
 
 }());
